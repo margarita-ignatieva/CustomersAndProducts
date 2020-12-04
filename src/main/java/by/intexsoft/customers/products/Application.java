7f688c6a54044e1783263f97ea01d6f5308c7ff8
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-12-04 19:05:06 +0300

    solution changed

--- src/main/java/by/intexsoft/customers/products/Application.java
+++ src/main/java/by/intexsoft/customers/products/Application.java
@@ -2,6 +2,7 @@ package by.intexsoft.customers.products;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Application {
     public static void main(String args[]) {
@@ -12,5 +13,22 @@ public class Application {
         mapUnOrdered.put("d", 1L);
         mapUnOrdered.put("e", 12L);
         System.out.println(MapToList.transformMapToList(mapUnOrdered, 10));
+        Map<String, Long> mapUnOrdered2 = new HashMap<>();
+        mapUnOrdered2.put("a", 101L);
+        mapUnOrdered2.put("b", 101L);
+        mapUnOrdered2.put("c", 500L);
+        mapUnOrdered2.put("d", 9000L);
+        mapUnOrdered2.put("e", 98L);
+        mapUnOrdered2.put("f", 98L);
+        System.out.println(MapToList.transformMapToList(mapUnOrdered2, 200));
+        Map<String, Long> mapOrdered = new TreeMap<>();
+        mapOrdered.put("a", 17L);
+        mapOrdered.put("b", 10L);
+        mapOrdered.put("c", 38L);
+        mapOrdered.put("d", 17L);
+        mapOrdered.put("e", 13L);
+        mapOrdered.put("f", 12L);
+        mapOrdered.put("g", 56L);
+        System.out.println(MapToList.transformMapToList(mapOrdered, 1));
     }
 }
--- src/main/java/by/intexsoft/customers/products/MapToList.java
+++ src/main/java/by/intexsoft/customers/products/MapToList.java
@@ -1,8 +1,10 @@
 package by.intexsoft.customers.products;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MapToList {
     public static List<List<String>> transformMapToList(
@@ -11,23 +13,34 @@ public class MapToList {
             throw new IllegalArgumentException("Map can't be null!");
         }
         List<List<String>> listOfCustomers = new ArrayList<>();
-        final List<String> tempCustomersList = new ArrayList<>();
+        List<String> tempCustomersList = new ArrayList<>();
         long currentSum = 0;
-        for (final Map.Entry<String, Long> entry : customersProducts.entrySet()) {
+        int step = 0;
+        Iterator<Map.Entry<String, Long>> entries = customersProducts.entrySet().iterator();
+        long sum = customersProducts.entrySet().stream().map(x -> x.getValue()).mapToLong(x -> x).sum();
+        List<String> customersDef = customersProducts.entrySet().stream().map(x -> x.getKey()).collect(Collectors.toList());
+        while (entries.hasNext()) {
+            step++;
+            Map.Entry<String, Long> entry = entries.next();
             tempCustomersList.add(entry.getKey());
             currentSum += entry.getValue();
             if (currentSum > threshold && tempCustomersList.size() > 1) {
-                listOfCustomers.add(new ArrayList<String>() {{
+                listOfCustomers.add(new ArrayList<>() {{
                     addAll(0, tempCustomersList.subList(0, tempCustomersList.size() -1));}});
                 tempCustomersList.clear();
                 tempCustomersList.add(entry.getKey());
                 currentSum = entry.getValue();
             }
             if (entry.getValue() >= threshold) {
-                listOfCustomers.add(new ArrayList<String>() {{add(entry.getKey());}});
+                listOfCustomers.add(new ArrayList<>() {{add(entry.getKey());}});
                 tempCustomersList.remove(entry.getKey());
                 currentSum = 0;
             }
+            if (sum <= threshold) {
+                listOfCustomers.add(customersDef.subList(step - 1, customersDef.size()));
+                break;
+            }
+            sum -= entry.getValue();
         }
         return listOfCustomers;
     }
